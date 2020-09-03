f6c44d26587235664287a2ef19155e6ca34b360c
Pedro <pedroberorato@gmail.com>
2020-09-03 16:24:50 -0300

    UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,8 +5,6 @@ module.exports = {
 
         const users = await User.findAll();
 
-        console.log('users: ', users)
-
         return response.json(users);
     },
 
@@ -20,5 +18,40 @@ module.exports = {
             return response.status(400).send({ error: err });
         }
         
+    },
+
+    async show(request, response) {
+
+        const { user_id } = request.params;
+
+        const user = await User.findByPk(user_id);
+
+        return response.json(user);        
+    },
+
+    async update(request, response) {
+
+        const { user_id } = request.params;
+        const { name, email } = request.body;
+
+        const user = await User.findByPk(user_id);
+
+        user.name = name;
+        user.email = email;
+
+        await user.save();
+
+        return response.json(user);        
+    },
+
+    async destroy(request, response) {
+
+        const { user_id } = request.params;
+
+        const user = await User.findByPk(user_id);
+
+        await user.destroy();
+
+        return response.json({ message: "User successfully deleted!" });        
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,11 @@ const routes = express.Router();
 
 const UserController = require('./controllers/UserController');
 
-
-routes.get('/', (req, res) => {
-    return res.send("Hi")
-});
-
+//User
 routes.get('/users', UserController.index );
-// routes.post('/users', UserController.store );
+routes.post('/users', UserController.store );
+routes.get('/users/:user_id', UserController.show );
+routes.put('/users/:user_id', UserController.update );
+routes.delete('/users/:user_id', UserController.destroy );
 
 module.exports = routes;
