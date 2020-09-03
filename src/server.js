99a71743c78678f5a7709dc3a37ff9936a8c1dfb
Pedro <pedroberorato@gmail.com>
2020-09-03 11:20:41 -0300

    Migrations

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
 
+
 //Start App
 const app = express();
-
 //Enable JSON
 app.use(express.json());
 //Routes
 app.use(routes);
-
 //Listener
 app.listen(3333);
